855692cba55c5c446f1622a5b5f392c953ec3894
igneefleur <igneefleur@gmail.com>
2021-12-09 23:02:48 +0100

    adding private message to GUI

--- src/Client.java
+++ src/Client.java
@@ -43,8 +43,11 @@ import org.xml.sax.SAXException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.awt.BorderLayout;
+
 ////// COLOR
 import java.awt.Color;
+
+
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Label;
@@ -269,7 +272,7 @@ private static final Color message_color = new Color(230, 230, 230);
 ////////////////////////////////////////////////////////////////////////////////
 //// MESSAGE ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-private class Message extends JPanel {
+private final class Message extends JPanel {
 	
 /*-----------------------------------------------------------------------------|
 ||----------------|----------------|------------------------------------------||
@@ -286,8 +289,10 @@ private class Message extends JPanel {
 ////////////////////////////////////////////////////////////////////
 		
 private final Panel head = new Panel();
-private final JLabel label_name;
-private final JLabel label_date;
+private final JLabel sender_label;
+private final JLabel arrow_label = new JLabel("  ->  ");
+private final JLabel receiver_label;
+private final JLabel date_label;
 private final JLabel space = new JLabel("  ");
 
 private final JTextArea textarea_message;
@@ -295,9 +300,14 @@ private final JTextArea textarea_message;
 ////////////////////////////////////////////////////////////////////
 //// ATTRIBUTES ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
-		
-private final String name;
-private final Color name_color;
+
+private final boolean is_public;
+
+private final String sender_name;
+private final Color sender_color;
+
+private final String receiver_name;
+private final Color receiver_color;
 
 private final String date;
 
@@ -309,36 +319,61 @@ private boolean is_encrypted = false;
 ////////////////////////////////////////////////////////////////////
 //// CONSTRUCTOR ///////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
-public Message(String name, String date, Color name_color, String message, String encrypted_message){
-
-
-	this.name = name;
+public Message(boolean is_public, String sender_name, Color sender_color, String receiver_name, Color receiver_color, String date, String message, String encrypted_message){
+	
+	
+	this.is_public = is_public;
+	
+	this.sender_name = sender_name;
+	this.sender_color = sender_color;
+	
+	this.receiver_name = receiver_name;
+	this.receiver_color = receiver_color;
+	
 	this.date = date;
-	this.name_color = name_color;
+	
 	this.message = message;
 	this.encrypted_message = encrypted_message;
 	
 	this.setLayout(new BorderLayout());
+
+////// HEAD
+	head.setLayout(new BoxLayout(head, BoxLayout.X_AXIS));
 	
-	label_name = new JLabel(name);
-	label_name.setAlignmentX(LEFT_ALIGNMENT);
-	label_name.setAlignmentY(CENTER_ALIGNMENT);
-	
-	label_date = new JLabel(date);
-	label_date.setAlignmentX(LEFT_ALIGNMENT);
-	label_date.setAlignmentY(CENTER_ALIGNMENT);
+	sender_label = new JLabel(sender_name);
+	sender_label.setAlignmentX(LEFT_ALIGNMENT);
+	sender_label.setAlignmentY(CENTER_ALIGNMENT);
+	sender_label.setForeground(sender_color);
+	head.add(sender_label);
+
+
+	receiver_label = new JLabel();
+	if(!is_public) {
+		arrow_label.setAlignmentX(LEFT_ALIGNMENT);
+		arrow_label.setAlignmentY(CENTER_ALIGNMENT);
+		arrow_label.setForeground(message_color);
+		head.add(arrow_label);
+		
+		receiver_label.setText(receiver_name);
+		receiver_label.setAlignmentX(LEFT_ALIGNMENT);
+		receiver_label.setAlignmentY(CENTER_ALIGNMENT);
+		receiver_label.setForeground(receiver_color);
+		head.add(receiver_label);	
+	}
 	
 	space.setAlignmentX(LEFT_ALIGNMENT);
 	space.setAlignmentY(CENTER_ALIGNMENT);
-	
-	head.setLayout(new BoxLayout(head, BoxLayout.X_AXIS));
-	head.add(label_name);
 	head.add(space);
-	head.add(label_date);
+	
+	date_label = new JLabel(date);
+	date_label.setAlignmentX(LEFT_ALIGNMENT);
+	date_label.setAlignmentY(CENTER_ALIGNMENT);
+	date_label.setForeground(date_color);
+	head.add(date_label);
 	
 	this.add(head, BorderLayout.NORTH);
 	
-	// TEXT AREA
+////// TEXT AREA
 	textarea_message = new JTextArea(message, 1, 1);
 	textarea_message.setEditable(false);
 	textarea_message.setColumns(60);
@@ -349,12 +384,9 @@ public Message(String name, String date, Color name_color, String message, Strin
 	this.add(textarea_message, BorderLayout.SOUTH);
 	this.setMaximumSize(new Dimension(450, 100));
 	
-	// COLORS
+////// COLORS
 	this.setBackground(background_color);
 	
-	label_name.setForeground(name_color);
-	label_date.setForeground(date_color);
-
 	textarea_message.setBackground(background_color);
 	textarea_message.setForeground(message_color);
 	
@@ -363,7 +395,7 @@ public Message(String name, String date, Color name_color, String message, Strin
 ////////////////////////////////////////////////////////////////////
 //// SWITCH ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////
-public void setIsEncrypted(boolean is_encrypted) {
+public final void setIsEncrypted(boolean is_encrypted) {
 
 	
 	this.is_encrypted = is_encrypted;
@@ -404,70 +436,104 @@ public void setIsEncrypted(boolean is_encrypted) {
 ||----SCROLLPANE---------------------------------------------------------------|
 |----WINDOWCHAT---------------------------------------------------------------*/
 	
-	private final Client client;
+private final Client client;
 
-	private boolean is_encrypted = false;
-	private boolean is_cleaned = true;
-	
-	private final Panel body = new Panel();
-	
-	private final ScrollPane scroll_pane = new ScrollPane(ScrollPane.SCROLLBARS_ALWAYS);
-	private final Panel main = new Panel();
-	private final Label space = new Label();
-	
-	private final JTextArea input = new JTextArea();
-	
-	private final ArrayList<Message> messages = new ArrayList<Message>();
-	
-	public void addMessage(String name, String date, Color name_color, String text, String encrypted_text){
-		Message message = new Message(name, date, name_color, text, encrypted_text);
-		message.setAlignmentX(LEFT_ALIGNMENT);
-		message.setAlignmentY(BOTTOM_ALIGNMENT);
-		
-		messages.add(message);
-		main.add(message);
-		this.pack();
-	}
-	
-	public void setIsEncrypted(boolean is_encrypted){
-		this.is_encrypted = is_encrypted;
-		
-		for (Iterator<Message> iterator = messages.iterator(); iterator.hasNext();)
-			((Message) iterator.next()).setIsEncrypted(is_encrypted);
-	}
-	
-	public Window(Client client){
-		super();
-		
-		this.client = client;
-		
-		body.setLayout(new BorderLayout());
-		
-		main.setBackground(background_color);
-		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
-		main.add(space);
-		scroll_pane.add(main);
-		
-		input.setBackground(background_color);
-		input.setForeground(message_color);
-		input.setRows(3);
-		
-		body.add(scroll_pane, BorderLayout.CENTER);		
-		body.add(input, BorderLayout.SOUTH);
-		this.add(body);
+private boolean is_encrypted = false;
+private boolean is_cleaned = true;
 
-		this.setUndecorated(true);
-		this.pack();
-		
-		this.setSize(500, 500);
-		this.setResizable(true);
-		
-		this.addWindowListener(this);
-		input.addKeyListener(this);
-		
-		this.setVisible(true);
-	}
+private final Panel body = new Panel();
 
+private final ScrollPane scroll_pane = new ScrollPane(ScrollPane.SCROLLBARS_ALWAYS);
+private final Panel main = new Panel();
+private final Label space = new Label();
+
+private final JTextArea input = new JTextArea();
+
+private final ArrayList<Message> messages = new ArrayList<Message>();
+
+	
+////////////////////////////////////////////////////////////////////
+//// ADD PUBLIC MESSAGE ////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+public final void addPublicMessage(String sender_name, Color sender_color, String date, String text, String encrypted_text){
+	
+	
+	Message message = new Message(true, sender_name, sender_color, null, null, date, text, encrypted_text);
+	message.setAlignmentX(LEFT_ALIGNMENT);
+	message.setAlignmentY(BOTTOM_ALIGNMENT);
+	
+	messages.add(message);
+	main.add(message);
+	this.pack();
+	
+	
+}
+////////////////////////////////////////////////////////////////////
+//// ADD PRIVATE MESSAGE ///////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+public final void addPrivateMessage(String sender_name, Color sender_color, String receiver_name, Color receiver_color, String date, String text, String encrypted_text){
+
+	
+	Message message = new Message(false, sender_name, sender_color, receiver_name, receiver_color, date, text, encrypted_text);
+	message.setAlignmentX(LEFT_ALIGNMENT);
+	message.setAlignmentY(BOTTOM_ALIGNMENT);
+	
+	messages.add(message);
+	main.add(message);
+	this.pack();
+
+
+}
+////////////////////////////////////////////////////////////////////
+//// SWITCH ENCRYPTED\DECRYPTED ////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+public final void setIsEncrypted(boolean is_encrypted){
+
+	
+	this.is_encrypted = is_encrypted;
+	for (Iterator<Message> iterator = messages.iterator(); iterator.hasNext();)
+		((Message) iterator.next()).setIsEncrypted(is_encrypted);
+
+
+}
+////////////////////////////////////////////////////////////////////
+//// CONSTRUCTOR ///////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+public Window(Client client){
+	super();
+	
+	
+	
+	this.client = client;
+	
+	body.setLayout(new BorderLayout());
+	
+	main.setBackground(background_color);
+	main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+	main.add(space);
+	scroll_pane.add(main);
+	
+	input.setBackground(background_color);
+	input.setForeground(message_color);
+	input.setRows(3);
+	
+	body.add(scroll_pane, BorderLayout.CENTER);		
+	body.add(input, BorderLayout.SOUTH);
+	this.add(body);
+
+	this.setUndecorated(true);
+	this.pack();
+	
+	this.setSize(500, 500);
+	this.setResizable(true);
+	
+	this.addWindowListener(this);
+	input.addKeyListener(this);
+	
+	this.setVisible(true);
+	
+	
+}
 ////////////////////////////////////////////////////////////////////////////////
 //// EVENTS ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -721,9 +787,7 @@ private final void readMessage(String message, String encrypted_message) {
 		sender_color = new Color(Integer.parseInt(sender_red), Integer.parseInt(sender_green), Integer.parseInt(sender_blue));
 		
 		System.out.println(time + " | " + sender + " -> " + "everyone" + " : " + content);
-		
-		// TODO : CONNECTION WITH WINDOW like :
-		window.addMessage(sender, time, sender_color, content, encrypted_message);
+		window.addPublicMessage(sender, sender_color, time, content, encrypted_message);
 		break;
 	case "private":
 ////// PRIVATE MESSAGE
@@ -746,8 +810,7 @@ private final void readMessage(String message, String encrypted_message) {
 		receiver_color = new Color(Integer.parseInt(receiver_red), Integer.parseInt(receiver_green), Integer.parseInt(receiver_blue));
 
 		System.out.println(time + " | " + sender + " -> " + receiver + " : " + content);
-		// TODO : CONNECTION WITH WINDOW like :
-		// window.addPrivateMessage(sender, receiver, time, sender_color, receiver_color, content, encrypted_message);
+		window.addPrivateMessage(sender, sender_color, receiver, receiver_color, time, content, encrypted_message);
 		break;
 	default:
 		break;
