ff6afab4665d27ae9acf7dec35c759a1d39b15de
FloDarPie <flo.duzes@gmail.com>
2021-12-09 10:03:51 +0100

    correction des documents

--- src/CleAES.java
+++ src/CleAES.java
@@ -1,5 +1,3 @@
-package chat;
-
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,3 @@
-package chat;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
